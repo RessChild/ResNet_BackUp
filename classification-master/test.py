5dacbb3fde308891f4c8639b93925cf9ae142045
RessChild <39401832+RessChild@users.noreply.github.com>
2020-05-28 21:42:18 +0900

    새 코드 저장

--- classification-master/test.py
+++ classification-master/test.py
@@ -7,8 +7,11 @@ from learning.evaluators import AccuracyEvaluator as Evaluator
 
 
 """ 1. 원본 데이터셋을 메모리에 로드함 """
-root_dir = os.path.join('/', 'mnt', 'sdb2', 'Datasets', 'asirra')    # FIXME
+# @@ 데이터 로드를 위해선 여길 수정해야 함
+root_dir = os.path.join('./', 'datas', 'asirra')    # FIXME
 test_dir = os.path.join(root_dir, 'test')
+# @@ 데이터가 저장된 곳
+save_dir = os.path.join('./','result','nn_repair')
 
 # 테스트 데이터셋을 로드함
 X_test, y_test = dataset.read_asirra_subset(test_dir, one_hot=True)
@@ -22,15 +25,21 @@ print((test_set.labels[:, 1] == 0).sum(), (test_set.labels[:, 1] == 1).sum())
 
 
 """ 2. 테스트를 위한 하이퍼파라미터 설정 """
+# @@ 평균 이미지 위치 바뀌면 여기도 수정해야 함
+# 여기도 바꿈
 hp_d = dict()
-image_mean = np.load('/tmp/asirra_mean.npy')    # load mean image
+image_mean = np.load('./asirra_mean.npy')    # load mean image
 hp_d['image_mean'] = image_mean
 
 # FIXME: 테스트 관련 하이퍼파라미터
 hp_d['batch_size'] = 256
 hp_d['augment_pred'] = True
 
+# @@ 구간 확인용
+print('파라미터 기록 성공')
 
+
+# @@ gpu 관련해서 호출하다 터지는거같음 확인 필요
 """ 3. Graph 생성, 파라미터 로드, session 초기화 및 테스트 시작 """
 # 초기화 
 graph = tf.get_default_graph()
@@ -41,8 +50,9 @@ model = ConvNet([227, 227, 3], 2, **hp_d)
 evaluator = Evaluator()
 saver = tf.train.Saver()
 
+# @@ 이부분 수정함
 sess = tf.Session(graph=graph, config=config)
-saver.restore(sess, '/tmp/model.ckpt')   # 학습된 파라미터 로드 및 복원
+saver.restore(sess, 'save_dir'+'/model.ckpt')   # 학습된 파라미터 로드 및 복원
 test_y_pred = model.predict(sess, test_set, **hp_d)
 test_score = evaluator.score(test_set.labels, test_y_pred)
 
--- classification-master/train.py
+++ classification-master/train.py
@@ -6,26 +6,39 @@ import numpy as np
 import tensorflow.compat.v1 as tf
 from datasets import asirra as dataset
 from models.nn import AlexNet as ConvNet
+from models.nn import ResNet as rn
 from learning.optimizers import MomentumOptimizer as Optimizer
 from learning.evaluators import AccuracyEvaluator as Evaluator
 
+ 
+#영현이의 수정 데이터셋 
+
+from keras.datasets import cifar100
+
+(X_trainval, y_trainval), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
 
 """ 1. 원본 데이터셋을 메모리에 로드하고 분리함 """
+
 # @@ 데이터 파일이 존재하는 경로
+# 여길 수정할 것
+"""
 root_dir = os.path.join('./', 'datas', 'asirra')    # FIXME
 trainval_dir = os.path.join(root_dir, 'train')
-
-# @@ 경로 추가
-save_dir = os.path.join('./','result','testing') 
+"""
+# @@ 저장 경로 추가
+save_dir = os.path.join('./','result','nn_repair') 
 
 # 원본 학습+검증 데이터셋을 로드하고, 이를 학습 데이터셋과 검증 데이터셋으로 나눔
+"""
 X_trainval, y_trainval = dataset.read_asirra_subset(trainval_dir, one_hot=True)
 trainval_size = X_trainval.shape[0]
 val_size = int(trainval_size * 0.2)    # FIXME
 val_set = dataset.DataSet(X_trainval[:val_size], y_trainval[:val_size])
 train_set = dataset.DataSet(X_trainval[val_size:], y_trainval[val_size:])
-
+"""
 # 중간 점검
+"""
 print('Training set stats:')
 print(train_set.images.shape)
 print(train_set.images.min(), train_set.images.max())
@@ -34,13 +47,30 @@ print('Validation set stats:')
 print(val_set.images.shape)
 print(val_set.images.min(), val_set.images.max())
 print((val_set.labels[:, 1] == 0).sum(), (val_set.labels[:, 1] == 1).sum())
+"""
 
 
 """ 2. 학습 수행 및 성능 평가를 위한 하이퍼파라미터 설정 """
+# @@ 평균 이미지 저장 위치도 변경해야 할 듯
+# 수정 했음
+subtract_pixel_mean = True
+# Input image dimensions.
+input_shape = X_trainval.shape[1:]
+
+# Normalize data.
+X_trainval = X_trainval.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# If subtract pixel mean is enabled
+if subtract_pixel_mean:
+    x_train_mean = np.mean(X_trainval, axis=0)
+    X_trainval -= x_train_mean
+    x_test -= x_train_mean
 hp_d = dict()
-image_mean = train_set.images.mean(axis=(0, 1, 2))    # 평균 이미지
-np.save('/tmp/asirra_mean.npy', image_mean)   # 평균 이미지를 저장
-hp_d['image_mean'] = image_mean
+
+#image_mean = train_set.images.mean(axis=(0, 1, 2))    # 평균 이미지
+#np.save('./asirra_mean.npy', image_mean)   # 평균 이미지를 저장
+hp_d['image_mean'] = x_train_mean
 
 # FIXME: 학습 관련 하이퍼파라미터
 hp_d['batch_size'] = 256
@@ -62,6 +92,8 @@ hp_d['dropout_prob'] = 0.5
 # FIXME: 성능 평가 관련 하이퍼파라미터
 hp_d['score_threshold'] = 1e-4
 
+# @@ 추가해준 파라미터 (층 수)
+hp_d['layer_cnt'] = 32
 
 """ 3. Graph 생성, session 초기화 및 학습 시작 """
 # 초기화
@@ -69,7 +101,7 @@ graph = tf.get_default_graph()
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 
-model = ConvNet([227, 227, 3], 2, **hp_d)
+model = ResNet([227, 227, 3], 2, **hp_d)
 evaluator = Evaluator()
 optimizer = Optimizer(model, train_set, evaluator, val_set=val_set, **hp_d)
 
